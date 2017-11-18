2b879906727e34d1658e7f86404491c3cd94f013
Karina T <karinat363@gmail.com>
2017-11-18 15:14:01 +0000

    Add users index view and action details

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -2,6 +2,6 @@ class Profile < ActiveRecord::Base
   belongs_to :user
   has_attached_file :avatar, 
                     :styles => { :medium => "300x300>", :thumb => "100x100>" }, 
-                    :default_url => "/images/:style/missing.png"
+                    :default_url => "/images/:style/missing.jpg"
   validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
 end
